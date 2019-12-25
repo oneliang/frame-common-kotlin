c52ff040797725216dfe3693b3ed69ba7abd3e5a
oneliang <582199098@qq.com>
2019-12-25 14:30:46 +0800

    optimize JdbcExtensions.kt

--- src/main/kotlin/com/oneliang/ktx/frame/jdbc/JdbcExtensions.kt
+++ src/main/kotlin/com/oneliang/ktx/frame/jdbc/JdbcExtensions.kt
@@ -2,18 +2,18 @@ package com.oneliang.ktx.frame.jdbc
 
 import com.oneliang.ktx.Constants
 
-private fun <T> defaultSqlConditionTransform(instance: T): String {
-    return Constants.Symbol.SINGLE_QUOTES + instance + Constants.Symbol.SINGLE_QUOTES
+private fun <T> sqlConditionTransform(instance: T): String {
+    return Constants.Symbol.SINGLE_QUOTES + instance.toString() + Constants.Symbol.SINGLE_QUOTES
 }
 
-fun <T> Iterable<T>.toSqlCondition(transform: (T) -> String = ::defaultSqlConditionTransform): String {
+fun <T, R> Iterable<T>.toSqlCondition(transform: (T) -> R): String {
     return this.joinToString {
-        transform(it)
+        sqlConditionTransform(transform(it))
     }
 }
 
-fun <T> Array<T>.toSqlCondition(transform: (T) -> String = ::defaultSqlConditionTransform): String {
+fun <T, R> Array<T>.toSqlCondition(transform: (T) -> R): String {
     return this.joinToString {
-        transform(it)
+        sqlConditionTransform(transform(it))
     }
 }
