0af01a5cdf3b6f796299b3590016f640fc3e293a
oneliang <582199098@qq.com>
2019-12-25 12:10:50 +0800

    +JdbcExtensions.kt

---
+++ src/main/kotlin/com/oneliang/ktx/frame/jdbc/JdbcExtensions.kt
@@ -0,0 +1,18 @@
+package com.oneliang.ktx.frame.jdbc
+
+import com.oneliang.ktx.Constants
+
+private fun <T> defaultSqlConditionTransform(instance: T): String {
+    return when (instance) {
+        is Int, Long -> instance.toString()
+        else ->
+            Constants.Symbol.SINGLE_QUOTES + instance + Constants.Symbol.SINGLE_QUOTES
+
+    }
+}
+
+fun <T> Iterable<T>.toSqlCondition(transform: (T) -> String = ::defaultSqlConditionTransform): String {
+    return this.joinToString {
+        transform(it)
+    }
+}
