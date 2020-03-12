a554c93dc40332cb5c2d151478e42e5c47462422
oneliang <582199098@qq.com>
2020-03-12 10:15:48 +0800

    optimize code

--- src/main/kotlin/com/oneliang/ktx/frame/jdbc/mysql/MySqlUtil.kt
+++ src/main/kotlin/com/oneliang/ktx/frame/jdbc/mysql/MySqlUtil.kt
@@ -4,8 +4,8 @@ import com.oneliang.ktx.Constants
 import com.oneliang.ktx.frame.jdbc.SqlUtil
 
 object MySqlUtil {
-    fun selectPaginationSql(table: String, condition: String = Constants.String.BLANK, sequenceKey: String, startSequence: String, rowPerPage: Int): String {
-        val paginationCondition = "$condition AND $sequenceKey > $startSequence ${Constants.Database.MySql.PAGINATION} 0,$rowPerPage"
+    fun selectPaginationSql(table: String, condition: String = Constants.String.BLANK, sequenceKey: String, startSequence: String, orderBy: String, rowPerPage: Int): String {
+        val paginationCondition = "$condition AND $sequenceKey > $startSequence $orderBy ${Constants.Database.MySql.PAGINATION} 0,$rowPerPage"
         return SqlUtil.selectSql(table = table, condition = paginationCondition)
     }
 }
